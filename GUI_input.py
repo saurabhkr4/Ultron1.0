908559db2d1d2e779435c657524a5af83ddf5af8
saurabhkr4 <97940574+saurabhkr4@users.noreply.github.com>
2022-03-07 00:25:17 +0530

    Modified

--- GUI_input.py
+++ GUI_input.py
@@ -397,43 +397,6 @@ btn_print = Button(root, text="Print",bg = "Cyan4", fg="white",
  activebackground="LightCyan3",command=lambda:print_area(text_area.get('1.0',END).splitlines())) #activebackground="LightCyan3",command=lambda:print_area(text_area.get('1.0',END).splitlines()))
 btn_print.place(x=380,y=500)
 
-#text_area.tag_configure("red", foreground="red")
-# apply the tag "red" 
-#text_area.highlight_pattern("word", "red")
-# img = PhotoImage(file="C:/Users/saura/OneDrive/Desktop/FileDialogue/pik2.png")
-# download = Button(root, text="Download", image = img)
-# download.place(x=380,y=500)
-# download.pack()
-
-
-
-
-
-# class TextWidget(Text):
-#     def _init_(self, *args, **kwargs):
-#         Text._init_(self, *args, **kwargs)  # pass all args to superclass
-#         self.filename = ''
-#         self._filetypes = [
-#             ('Text', '*.txt'),
-#             ('All files', '*'),
-#         ]
-
-#     def save_file(self, whatever = None):
-#         if (self.filename == ''):
-#             self.save_file_as()
-#         else:
-#             f = open(self.filename, 'w')
-#             f.write(self.get('1.0', 'end')) # change every 'self' that refers to the Text, to self.text
-#             f.close()
-#             tkinter.messagebox.showinfo('FYI', 'File Saved.')
-
-#     def save_file_as(self, whatever = None):
-#         self.filename = tkinter.filedialog.asksaveasfilename(defaultextension='.txt',
-#                                                              filetypes = self._filetypes)
-#         f = open(self.filename, 'w')
-#         f.write(Text.get('1.0', END))
-#         f.close()
-#         tkinter.messagebox.showinfo('FYI', 'File Saved')   
 def save_file_as():
     filename = tkinter.filedialog.asksaveasfilename(defaultextension='.txt')
     f = open(filename, 'w')
